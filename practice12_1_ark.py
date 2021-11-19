75ac7dfc9477827375955df5c90ef87533ba1596
JonathanLopez42 <75470236+HojoJohn@users.noreply.github.com>
2021-11-19 14:36:59 -0500

    Add files via upload

---
+++ practice12_1_ark.py
@@ -0,0 +1,17 @@
+import random
+
+# Field Numbers
+NAME = 0
+SOUND = 1
+COLOR = 2
+
+ANIMALS = [
+    ["tiger","roar","orange/black"],
+    ["parrot","squawk","green"],
+    ["shark","chomp","grey"],
+    ["dog","ruff","brown"],
+    ["snake","hiss","red/black"],
+    ["sheep","bleat","white"],
+    ["cat","meow","black"],
+    ["mouse","squeak","grey"]
+]
